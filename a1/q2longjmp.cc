8ec63abe1b6ae57b869c8cd59c458c2132aff7c5
Spinspincoupling <wzq145@gmail.com>
2020-01-17 14:52:47 -0500

    q2

--- a1/q2longjmp.cc
+++ a1/q2longjmp.cc
@@ -1,8 +1,10 @@
 #include <iostream>
 #include <cstdlib>                                      // access: rand, srand
 #include <cstring>                                      // access: strcmp
-using namespace std;
 #include <unistd.h>                                     // access: getpid
+#include <setjmp.h>
+#include <vector>
+using namespace std;
 
 #ifdef NOOUTPUT
 #define PRT( stmt )
@@ -13,24 +15,53 @@ using namespace std;
 struct E {};                                            // exception type
 PRT( struct T { ~T() { cout << "~"; } }; )
 long int eperiod = 100, excepts = 0, calls = 0;        // exception period
+jmp_buf jumpE1,jumpE2,jumpE3;
+vector<int> stack;
+
+void throwException(){
+    if(stack.size() > 0){
+        if(stack.back() == 1){
+            longjmp(jumpE1);
+        } else {
+            longjmp(jumpE2);
+        }  
+    } else {
+        longjmp(jumpE3);
+    }  
+}
 
 long int Ackermann( long int m, long int n ) {
     calls += 1;
     if ( m == 0 ) {
-        if ( rand() % eperiod == 0 ) { PRT( T t; ) excepts += 1; throw E(); }
+        if ( rand() % eperiod == 0 ) { 
+            PRT( T t; ) excepts += 1; 
+            throwException(); 
+        }
         return n + 1;
     } else if ( n == 0 ) {
-        try { return Ackermann( m - 1, 1 );
-        } catch( E ) {
-            PRT( cout << "E1 " << m << " " << n << endl );
-            if ( rand() % eperiod == 0 ) { PRT( T t; ) excepts += 1; throw E(); }
-        } // try
+            if(setjmp(jumpE1) == 0) {
+                stack.emplace_back(1);
+                return Ackermann( m - 1, 1 );
+            } else {
+                stack.pop_back();
+                PRT( cout << "E1 " << m << " " << n << endl );
+                if ( rand() % eperiod == 0 ) { 
+                    PRT( T t; ) excepts += 1; 
+                    throwException();
+                }
+            }
     } else {
-        try { return Ackermann( m - 1, Ackermann( m, n - 1 ) );
-        } catch( E ) {
-            PRT( cout << "E2 " << m << " " << n << endl );
-            if ( rand() % eperiod == 0 ) { PRT( T t; ) excepts += 1; throw E(); }
-        } // try
+            if(setjmp(jumpE2) == 0){
+                stack.emplace_back(2);
+                return Ackermann( m - 1, Ackermann( m, n - 1 ) );
+            } else {
+                stack.pop_back();
+                PRT( cout << "E2 " << m << " " << n << endl );
+                if ( rand() % eperiod == 0 ) { 
+                    PRT( T t; ) excepts += 1; 
+                    throwException();
+                }
+            }
     } // if
     return 0;                                           // recover by returning 0
 }
@@ -59,12 +90,12 @@ int main( int argc, char * argv[] ) {
         exit( EXIT_FAILURE );
     } // try
     srand( seed );                                      // seed random number
-    try {                                               // begin program
+    if (setjmp(jumpE3) == 0){                                              // begin program
         PRT( cout << m << " " << n << " " << seed << " " << eperiod << endl );
         long int val = Ackermann( m, n );
         PRT( cout << val << endl );
-    } catch( E ) {
+    } else {
         PRT( cout << "E3" << endl );
-    } // try
+    }
     cout << "calls " << calls << ' ' << " exceptions " << excepts << endl;
 }
