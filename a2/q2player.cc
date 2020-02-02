76e9648e636e6414bfb71af871897980ef122591
Spinspincoupling <wzq145@gmail.com>
2020-02-02 17:05:24 -0500

    remove all comments

--- a2/q2player.cc
+++ a2/q2player.cc
@@ -16,7 +16,6 @@ void Player::passDeck(unsigned int deck){
 }
 
 void Player::main(){
-    //std::cout << "num: " << numPlayers << std::endl;
     suspend(); //start set cocaller
     //play get deck
     for(;;) {
@@ -24,13 +23,11 @@ void Player::main(){
                 _Enable{}
         }
         catch (Schmilblick &){
-            //std::cout << "catch at " << id << std::endl;
             printer.prt(id, 0, -1);
             _Resume Schmilblick() _At *rplayer;
             rplayer->drink();
             continue;
         }
-        //std::cout << "remaining players: " << numPlayers << std::endl;
         if(numPlayers == 1){ //win
             printer.prt(id, 0, numPlayers);
             _Resume GameOver() _At starter();
@@ -38,22 +35,18 @@ void Player::main(){
         }
         unsigned int take = prng(1, 8); 
         if(take >= deck) { //win
-        //std::cout << "should end" << std::endl;
             printer.prt(id, deck, numPlayers);
             _Resume GameOver() _At starter();
             return;
         }
-        //std::cout << "take and not win at " << id << " count: " << take << std::endl; 
         printer.prt(id, take, numPlayers);
         if(deck%DEATH_DECK_DIVISOR == 0){ //check dead
             deck -= take;
             terminate();
-            //std::cout << "one die" << std::endl;
             break;
         }
         try {
             if(prng(9) == 0){
-                //std:: cout << "start drink" <<" raise at " << id << std::endl;
                 printer.prt(id, 0, -1);
                 _Resume Schmilblick() _At *rplayer;
                 rplayer->drink();
@@ -61,7 +54,6 @@ void Player::main(){
             }
         }
         catch (Schmilblick &){
-            //std::cout << "return catch at " << id << std::endl;
         }
         deck -= take;
         passDeck(deck);    
