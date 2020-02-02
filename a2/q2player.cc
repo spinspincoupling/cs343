3b6ee3069898ce1eaae839f597da52bfe51ae0a2
Spinspincoupling <wzq145@gmail.com>
2020-02-02 14:42:01 -0500

    debug

--- a2/q2player.cc
+++ a2/q2player.cc
@@ -31,7 +31,7 @@ void Player::main(){
         unsigned int take = prng(1, 8); 
         try {
             if(deck%DEATH_DECK_DIVISOR != 0 && prng(9) == 0){
-                std:: cout << "start drink" << std::endl;
+                std:: cout << "start drink" <<" count is " << take << std::endl;
                 std::cout << "call to printer -1" << std::endl;
                 printer.prt(id, -1, -1);
                 _Resume Schmilblick() _At *rplayer;
