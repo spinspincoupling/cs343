b7a44bfc6603faea716b18fb98dbd85300fb74f8
Spinspincoupling <wzq145@gmail.com>
2020-02-03 17:06:10 -0500

    remove comment

--- a2/q2printer.cc
+++ a2/q2printer.cc
@@ -4,10 +4,7 @@
 void Printer::clearBuffer(){
     int lastwrite = 0;
     for(unsigned int i=0; i<NoOfPlayers; ++i){
-            if(!written[i]){
-                //if(i < NoOfPlayers-1) std::cout << '\t';
-                continue;
-            }
+            if(!written[i]) continue;
             written[i] = false;
             for(unsigned int j=lastwrite; j<i; ++j){
                 std::cout << '\t';
@@ -15,7 +12,6 @@ void Printer::clearBuffer(){
             lastwrite = i;
             if(remainingPlayers.get()[i] == -1){
                 std::cout << 'D' ;
-                //if(i < NoOfPlayers-1) std::cout<< '\t';
                 continue;
             }
             bool dead = (taken[i]+remainingCards[i]) % DEATH_DECK_DIVISOR == 0;
@@ -26,8 +22,7 @@ void Printer::clearBuffer(){
                 if(remainingCards[i]%2 == 0) std::cout << '>';
                 else std::cout << '<';
             } else std::cout << '#';
-            if(dead) std::cout << 'X';
-            //if(i < NoOfPlayers-1)  std::cout << '\t';           
+            if(dead) std::cout << 'X';         
         }
     std::cout << std::endl;
 }
