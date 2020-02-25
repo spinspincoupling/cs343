bc3b9d8ecf321720a5c09fc05745f8190277e04d
Spinspincoupling <wzq145@gmail.com>
2020-02-25 11:06:39 -0500

    debug msg

--- a3/q2polymult.cc
+++ a3/q2polymult.cc
@@ -62,6 +62,7 @@ void polymultiply( const poly_t & a, const poly_t & b, poly_t & c, const size_t 
 
     uActorStart();					// start actor system
     for(unsigned int i=0; i<delta; ++i){
+        std::cout << "here";
         WorkMsg *msg = new WorkMsg(delta, i, a, b, c);
         if (msg == nullptr) std::cout << "null";
         Multiply() | *msg;
