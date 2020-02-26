53fab7f26d1cb8cd3460291fe6f588fd7f28a8fb
Spinspincoupling <wzq145@gmail.com>
2020-02-25 19:39:19 -0500

    indent

--- a3/q2main.cc
+++ a3/q2main.cc
@@ -13,56 +13,54 @@ void inputpoly(char *fileA, char *fileB){
         file1.open(fileA);
         file2.open(fileB);
         int size1, size2;
-        while(file1 >> size1 && file2 >> size2){
-            int sizer = size1+size2-1;
-            //cout << size1 << " " << size2  << " " << sizer << endl;
-            int *poly1 = new int[size1];
-            int *poly2 = new int[size2];
-            int *polyr = new int[sizer];
-            for(int i=0; i<size1; ++i){//assume sizes are the same
-                file1 >> poly1[i];
-                file2 >> poly2[i];
-            }
-            Poly c = {polyr, (size_t)sizer};
-            polymultiply(Poly{poly1, (size_t)size1}, Poly{poly2, (size_t)size2}, c, floor(sqrt(sizer)));
-            for(int i=size1-1; i>0; --i){
-                if(poly1[i] != 0){
-                    cout << poly1[i] << "x^" << i << " + "; 
-                }
-            }
-            if(poly1[0] != 0){
-                cout << poly1[0] <<"x^" << 0;
-            }
-            cout << endl;
-            for(int i=size2-1; i>0; --i){
-                if(poly2[i] != 0){
-                    cout << poly2[i]<< "x^" << i << " + ";
-                }
-            }
-            if(poly2[0] != 0){
-                cout << poly2[0] <<"x^" << 0;
-            }
-            cout << endl;
-            for(int i=sizer-1; i>0; --i){
-                if(polyr[i] != 0){
-                    cout << polyr[i]<< "x^" << i << " + ";
-                }
-            }
-            if(polyr[0] != 0){
-                cout << polyr[0] <<"x^" << 0;
-            }
-            cout << endl;
-            delete poly1;
-            delete poly2;
-            delete polyr;
+        file1 >> size1;
+        file2 >> size2;
+        int sizer = size1 + size2 - 1;
+        int *poly1 = new int[size1];
+        int *poly2 = new int[size2];
+        int *polyr = new int[sizer];
+        for (int i = 0; i < size1; ++i){ //assume sizes are the same
+            file1 >> poly1[i];
+            file2 >> poly2[i];
         }
+        Poly c = {polyr, (size_t)sizer};
+        polymultiply(Poly{poly1, (size_t)size1}, Poly{poly2, (size_t)size2}, c, floor(sqrt(sizer)));
+        for (int i = size1 - 1; i > 0; --i){
+            if (poly1[i] != 0){
+                cout << poly1[i] << "x^" << i << " + ";
+            }
+        }
+        if (poly1[0] != 0){
+            cout << poly1[0] << "x^" << 0;
+        }
+        cout << endl;
+        for (int i = size2 - 1; i > 0; --i){
+            if (poly2[i] != 0){
+                cout << poly2[i] << "x^" << i << " + ";
+            }
+        }
+        if (poly2[0] != 0){
+            cout << poly2[0] << "x^" << 0;
+        }
+        cout << endl;
+        for (int i = sizer - 1; i > 0; --i){
+            if (polyr[i] != 0){
+                cout << polyr[i] << "x^" << i << " + ";
+            }
+        }
+        if (polyr[0] != 0){
+            cout << polyr[0] << "x^" << 0;
+        }
+        cout << endl;
+        delete poly1;
+        delete poly2;
+        delete polyr;
         file1.close();
         file2.close();
     }
     catch(exception &e){
         file1.close();
         file2.close();
-        cerr << e.what() << endl;
         cerr << "Error! Could not open input files \"" << fileA << "\" \"" << fileB << "\"" << endl;
         exit( EXIT_FAILURE );
     }
