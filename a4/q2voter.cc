adc899862db69cf6ac010698994eb232c63ac495
Spinspincoupling <wzq145@gmail.com>
2020-03-09 02:48:44 -0400

    syntax

--- a4/q2voter.cc
+++ a4/q2voter.cc
@@ -4,14 +4,14 @@
 
 extern MPRNG mprng;
 
-Voter::Ballot cast() {        // cast 3-way vote
+TallyVotes::Ballot Voter::cast() {        // cast 3-way vote
         // O(1) random selection of 3 items without replacement using divide and conquer.
         static const unsigned int voting[3][2][2] = { { {2,1}, {1,2} }, { {0,2}, {2,0} }, { {0,1}, {1,0} } };
         unsigned int picture = mprng( 2 ), statue = mprng( 1 );
         return (TallyVotes::Ballot){ picture, voting[picture][statue][0], voting[picture][statue][1] };
     }
 
-Voter::main(){
+void Voter::main(){
     try {
         while(nvotes > 0){
         yield(mprng(19));
