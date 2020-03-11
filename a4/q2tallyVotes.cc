8c1d7e624075e24536ea40c29ba829a15c5307a4
Spinspincoupling <wzq145@gmail.com>
2020-03-10 20:25:47 -0400

    delete comment

--- a4/q2tallyVotes.cc
+++ a4/q2tallyVotes.cc
@@ -32,13 +32,13 @@ void TallyVotes::computeTour(){
             mutex.release();
             throw Failed();
         }
-        std::cout << "first check" << signalled << std::endl;
-        if(signalled > 0 || groupMem == group){ //barger
+        //std::cout << "first check" << signalled << std::endl;
+        if(signalled > 0){ //barger
             ++barger;
             printer.print(id, Voter::States::Barging, barger);
             waitVote.wait(mutex);
             --barger;
-            std::cout << "woke up from barger" << signalled << std::endl;
+            //std::cout << "woke up from barger " << signalled << std::endl;
             --signalled;
             if(voters < group) { // quorum failure
                 mutex.release();
@@ -51,7 +51,7 @@ void TallyVotes::computeTour(){
             computeTour();
             signalled += group-1;
             waitVoters.broadcast();
-            std::cout << "signal group" << signalled << std::endl;
+            //std::cout << "signal group " << signalled << std::endl;
             printer.print(id, Voter::States::Complete, Tour{kind, groupNum});
         } else {
             printer.print(id, Voter::States::Block, groupMem);
@@ -60,7 +60,7 @@ void TallyVotes::computeTour(){
             --waiting;
             printer.print(id, Voter::States::Unblock, waiting);
             --signalled;
-            std::cout << "awake from wait vote" << signalled << std::endl;
+            //std::cout << "awake from wait vote " << signalled << std::endl;
         }
         if(voters < group) { // quorum failure
                 mutex.release();
@@ -70,7 +70,7 @@ void TallyVotes::computeTour(){
         if(takeTour == group){
             takeTour = 0;
         }
-        if(waitVote.signal()) ++signalled;
+        //if(waitVote.signal()) ++signalled;
         //std::cout << signalled << std::endl;
         Tour tour = {kind, groupNum};
         mutex.release();
