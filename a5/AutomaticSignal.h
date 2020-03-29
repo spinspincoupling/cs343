b6dd2410b11c6b1e2445a4f321558995c5ae1b93
Spinspincoupling <wzq145@gmail.com>
2020-03-29 17:14:26 -0400

    try

--- a5/AutomaticSignal.h
+++ a5/AutomaticSignal.h
@@ -1,27 +1,25 @@
 #define AUTOMATIC_SIGNAL
-    #include <vector>
-    std::vector<uCondition> signalled;
+    #include <vector> \
+    std::vector<uCondition> signalled; \
 
-#define WAITUNTIL( pred, before, after ) {
-    if(!(pred)){
-        before;
-        int index = signalled.size();
-        uCondition blocked;
-        blocked.wait();
-        signalled.emplace_back(blocked;
-        while(!(pred)){
-            if(index+1 < signalled.size()) signalled[index+1].signal();
-            signalled[index].wait();
-        }
-        signalled.erase(index);
-        after;
-    }
-}
+#define WAITUNTIL( pred, before, after ) \
+    if(!(pred)){ \
+        before; \
+        int index = signalled.size(); \
+        uCondition blocked; \
+        blocked.wait(); \
+        signalled.emplace_back(blocked; \
+        while(!(pred)){ \
+            if(index+1 < signalled.size()) signalled[index+1].signal(); \
+            signalled[index].wait(); \
+        } \
+        signalled.erase(index); \
+        after; \
+    } \
     
     
-#define EXIT() {
-    if(signalled.size() > 0){
-        signalled[0].signal();
-    }
-}
+#define EXIT()
+    if(signalled.size() > 0){ \
+        signalled[0].signal(); \
+    } \
     
