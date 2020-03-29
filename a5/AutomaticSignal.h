11cd39b1726d73794eab9a3e1a5b71c454cc31b5
Spinspincoupling <wzq145@gmail.com>
2020-03-29 18:44:42 -0400

    use a single cond var

--- a5/AutomaticSignal.h
+++ a5/AutomaticSignal.h
@@ -1,34 +1,23 @@
 #ifndef AUTOMATICSIGNAL_H
 #define AUTOMATICSIGNAL_H
-#include <vector>
 
 #define AUTOMATIC_SIGNAL \
-        std::vector<uCondition*> signalledQueue; \
-        unsigned int current= 0
+        uCondition blocked
 
 #define WAITUNTIL( pred, before, after ) \
     if(!(pred)){ \
         before; \
-        auto index = signalledQueue.size(); \
-        uCondition *blocked = new uCondition(); \
-        signalledQueue.emplace_back(blocked); \
-        signalledQueue[index]->wait(); \
+        blocked.wait() \
         while(!(pred)){ \
-            ++current; \
-            if(current < signalledQueue.size()) signalledQueue[current]->signal(); \
-            signalledQueue[current-1]->wait(); \
+            if(!blocked.empty()) blocked.signal(); \
+            blocked.wait(); \
         } \
-        delete signalledQueue[current]; \
-        signalledQueue.erase(signalledQueue.begin()+current); \
         after; \
     } 
     
     
 #define EXIT() \
-    if(signalledQueue.size() > 0){ \
-        current = 0;\
-        signalledQueue[0]->signal(); \
-    }
+    if(!blocked.empty()) blocked.signal(); \
 
 #endif
     
