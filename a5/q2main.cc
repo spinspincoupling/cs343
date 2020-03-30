b37eb4289e2a3edfb5b2ded44e98bdbe003831ea
Spinspincoupling <wzq145@gmail.com>
2020-03-30 12:45:25 -0400

    add print seed

--- a5/q2main.cc
+++ a5/q2main.cc
@@ -39,6 +39,7 @@ int main( int argc, char * argv[] ) {
     } // try
     uProcessor p[processors - 1];
     mprng.set_seed(seed);
+    std::cout << "seed is " << seed << std::end;
     Printer printer(voters);
     TallyVotes voteTallier(voters, group, printer);
     Voter * voterlist[voters];
