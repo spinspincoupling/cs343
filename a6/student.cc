a75692c4972200e1bddd9b870a074c5f5ba71c08
Spinspincoupling <wzq145@gmail.com>
2020-04-10 21:51:59 -0400

    remove print

--- a6/student.cc
+++ a6/student.cc
@@ -124,7 +124,7 @@ void Student::main(){
             }*/
             prt.print(Printer::Kind::Student, id, 'W', start);
             //Train *train = stop->wait(id, dir);
-            prt.print(Printer::Kind::Student, id, 'E', train->getId());
+            //prt.print(Printer::Kind::Student, id, 'E', train->getId());
             //stop = train->embark(id, end, *cardUsing);
             prt.print(Printer::Kind::Student, id, 'D', end);
             //stop->disembark(id);
