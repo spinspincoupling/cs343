72655e8269d6fcfd9afe9f0301e99059044e83ca
Spinspincoupling <wzq145@gmail.com>
2020-04-12 01:44:55 -0400

    add msg

--- a6/conductor.cc
+++ a6/conductor.cc
@@ -1,12 +1,14 @@
 #include "conductor.h"
 #include "train.h"
 #include "printer.h"
+#include <iostream>
 
 Conductor::Conductor( Printer & prt, unsigned int id, Train * train, unsigned int delay )
     :prt{prt}, id{id}, train{train}, delay{delay}{}
 
 Conductor::~Conductor(){
     prt.print(Printer::Kind::Conductor, id, 'F');
+    std::cout << "end conductor destructor" << '\n' ;
 }
 
 void Conductor::main(){
@@ -21,4 +23,5 @@ void Conductor::main(){
             train->scanPassengers();
         }
     }
+    std::cout << "breaking out of conductor destructor" << '\n';
 }
