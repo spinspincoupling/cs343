4c191bd6ec900d109941d6f7a36a9954e9cc8329
Spinspincoupling <wzq145@gmail.com>
2020-04-13 00:01:49 -0400

    use correct method

--- a6/train.cc
+++ a6/train.cc
@@ -53,7 +53,7 @@ void Train::scanPassengers(){
             }
         }
     } else {
-        ((Conductor*)(&uBaseTask())->active = false;
+        ((Conductor) uThisTask())->active = false;
     }
     
 }
