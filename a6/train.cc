48493b3f58efe895730086f285fa0253fc4218be
Spinspincoupling <wzq145@gmail.com>
2020-04-13 00:02:45 -0400

    syntax

--- a6/train.cc
+++ a6/train.cc
@@ -53,7 +53,7 @@ void Train::scanPassengers(){
             }
         }
     } else {
-        ((Conductor) uThisTask())->active = false;
+        ((Conductor*) &uThisTask())->active = false;
     }
     
 }
